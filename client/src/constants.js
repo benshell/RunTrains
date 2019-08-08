6285478d6f8d2e802fc6d174d5046b5537b792ac
Ben Shell <benjaminshell@gmail.com>
2019-08-08 12:12:02 -0700

    Fix issue with disabling SSL via env var

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,3 +1,6 @@
 export const SERVER_HOST = process.env.REACT_APP_SERVER_HOST || 'localhost'
 export const SERVER_PORT = process.env.REACT_APP_SERVER_PORT || '8000'
-export const SERVER_SSL = process.env.REACT_APP_SERVER_SSL || false
+export const SERVER_SSL =
+  (process.env.REACT_APP_SERVER_SSL &&
+    process.env.REACT_APP_SERVER_SSL !== 'false') ||
+  false
